328652c422e0a52c077351894eff92877fc5b766
Edgar Leon Sandoval <leon.s.edgar@tec.mx>
2026-07-01 10:08:07 -0700

    fixed the first new

--- news/news-data.js
+++ news/news-data.js
@@ -4,13 +4,13 @@
 */
 window.QUACKENIO_NEWS = [
   {
-    id: "2026-06-30-promo-mexico-ecuador",
-    date: "2026-06-30",
+    id: "2026-07-01-promo-mexico-ecuador",
+    date: "2026-07-01",
     title: "Promo especial: Mexico vs Ecuador",
     category: "Promociones",
-    image: "news/300626promo_mexico_ecuador.jpeg",
+    image: "news/010726promo_mexico_ecuador.jpeg",
     imageAlt: "Promocion Quackenio por el partido Mexico vs Ecuador",
     excerpt:
-      "Ya publicamos la primera noticia en Quackenio: una promocion especial por el partido Mexico vs Ecuador. Ven por tu cafe y pregunta por la promo vigente."
+      "México invicto: una promocion especial por el partido Mexico vs Ecuador. Ven por tu cafe y dí quack."
   }
 ];
